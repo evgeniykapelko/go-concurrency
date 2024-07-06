de978b94d8c5b2431de0278e18a0a21a56da4f84
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-07-06 17:36:15 +0300

    Work with wait group,

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println(s)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"theta",
+		"epsilon",
+	}
+
+	wg.Add(len(words))
+
+	for i, word := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, word), &wg)
+	}
+
+	wg.Wait()
+
+	//time.Sleep(1 * time.Second)
+
+	wg.Add(1)
+
+	printSomething("This is the second thing to do so", &wg)
+}
